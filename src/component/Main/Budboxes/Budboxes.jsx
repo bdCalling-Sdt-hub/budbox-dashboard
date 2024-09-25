2ae27357c4ba4fa215e0e5677b967154824a0183
rakibislam2233 <rakib2020.tkg@gmail.com>
2024-09-25 10:41:59 +0600

    Bud box details component updated

--- src/component/Main/Budboxes/Budboxes.jsx
+++ src/component/Main/Budboxes/Budboxes.jsx
@@ -17,15 +17,15 @@ const Budboxes = () => {
   return (
     <section>
       <div className="w-full flex justify-between items-center px-3 py-6">
-        <h1 className="text-xl font-semibold ">Budboxes</h1>
+        <h1 className="text-2xl font-semibold ">Budboxes</h1>
         <Link to={`/budboxes/add-box`}>
-          <button className="px-8 py-2 bg-[#111111] text-white flex justify-center items-center gap-1 rounded text-sm">
+          <button className="px-10 py-3 bg-[#111111] text-white flex justify-center items-center gap-1 rounded text-sm">
             <FaPlus />
             Add Box
           </button>
         </Link>
       </div>
-      <div className="w-full grid grid-cols-1 md:grid-cols-4 gap-5">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
         {
           boxesData.map((item, index) => {
             return <BudboxesCard key={index + 1} item={item} />
--- src/component/Main/Budboxes/BudboxesCard.jsx
+++ src/component/Main/Budboxes/BudboxesCard.jsx
@@ -10,7 +10,7 @@ const BudboxesCard = ({ item }) => {
         <img
           src={image}
           alt={name}
-          className="w-full h-60 object-cover  rounded-md cursor-pointer "
+          className="w-full h-96 object-cover  rounded-md cursor-pointer "
         />
         <div className="absolute bg-black top-0 left-0 right-0 bottom-0 bg-opacity-25"></div>
         <div className="absolute left-3 bottom-5 gap-8 space-y-2">
@@ -18,11 +18,11 @@ const BudboxesCard = ({ item }) => {
             <h1 className="text-2xl font-semibold text-white">{name}</h1>
           </div>
           <div className="flex gap-5">
-            <button className="px-8 py-2 bg-[#f7cc50] text-white rounded text-sm">
+            <button className="px-10 py-3 bg-[#f7cc50] text-white rounded text-sm">
               Delete
             </button>
             <Link to={`/budboxes/edit-box/${123456}`}>
-              <button className="px-10 py-2 border border-[#f7cc50] text-white rounded text-sm">
+              <button className="px-12 py-3 border border-[#f7cc50] text-white rounded text-sm">
                 Edit
               </button>
             </Link>
